81ce99d44d7f9840afb30d5faba117800dd8cfbc
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-27 14:36:30 +0000

    Payment works

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -19,4 +19,24 @@ class CalcController < ApplicationController
     @the_result = @the_num**0.5
     render(:template => "omnicalc_template/square_root_results")
   end
+
+  def new_payment
+    render(:template => "omnicalc_template/new_payment")
+  end
+
+  def payment
+    @the_APR = params.fetch("users_APR").to_f
+    @the_years = params.fetch("users_year_number").to_f
+    @the_principal = params.fetch("users_principal").to_f
+    @percent_APR = @the_APR/100
+    @r = @percent_APR/12
+    @numerator = @r*@the_principal
+    @first = 1+ @r
+    @second = @first ** (-12*@the_years)
+    @denominator = 1 - @second
+    @the_result = @numerator/ @denominator
+    @the_result = @the_result.to_fs(:currency)
+    @the_APR = @the_APR.to_fs(:percentage, { :precision => 4 } ) 
+    render(:template => "omnicalc_template/payment_results")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get("/square/results", {:controller => "calc", :action => "square"})
   get("/square_root/new", {:controller => "calc", :action => "new_square_root"})
   get("/square_root/results", {:controller => "calc", :action => "square_root"})
+  get("/payment/new", {:controller => "calc", :action => "new_payment"})
+  get("/payment/results", {:controller => "calc", :action => "payment"})
   
 end
