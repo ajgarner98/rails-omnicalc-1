0d3150d47fc016c24f3e991beb8dedb4ec9ae673
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-27 14:20:59 +0000

    Fixed my square root

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -9,4 +9,14 @@ class CalcController < ApplicationController
     @the_result = @the_num**2
     render(:template => "omnicalc_template/square_results")
   end
+
+  def new_square_root
+    render(:template => "omnicalc_template/new_square_root")
+  end
+  
+  def square_root
+    @the_num = params.fetch("user_number").to_f
+    @the_result = @the_num**0.5
+    render(:template => "omnicalc_template/square_root_results")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get("/square/new", {:controller => "calc", :action=> "new_square"})
-  get("/square/results", {:controller => "calc", :action=> "square"})
+  get("/square/new", {:controller => "calc", :action => "new_square"})
+  get("/square/results", {:controller => "calc", :action => "square"})
+  get("/square_root/new", {:controller => "calc", :action => "new_square_root"})
+  get("/square_root/results", {:controller => "calc", :action => "square_root"})
   
 end
